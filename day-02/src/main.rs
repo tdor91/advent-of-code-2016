f721cf306c888a34b923935b5e2ca11a54d0601f
tdor91 <thomas.dorfer@gmx.net>
2025-06-01 19:57:10 +0200

    improve code quality

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -2,30 +2,17 @@ use day_02::{ComplexKeypad, Keypad, SimpleKeypad};
 
 fn main() {
     let input = include_str!("../input/input.txt");
-
     let lines: Vec<_> = input.split("\n").map(|x| x.trim()).collect();
 
-    println!("Part 1: {}", part1(&lines));
-    println!("Part 2: {}", part2(&lines));
+    println!("Part 1: {}", solve(&lines, SimpleKeypad::new()));
+    println!("Part 2: {}", solve(&lines, ComplexKeypad::new()));
 }
 
-fn part1(lines: &Vec<&str>) -> String {
+fn solve<T>(lines: &Vec<&str>, mut keypad: T) -> String
+where
+    T: Keypad,
+{
     let mut buttons: Vec<char> = vec![];
-    let mut keypad = SimpleKeypad::new();
-
-    for line in lines {
-        buttons.push(keypad.process_line(line));
-    }
-
-    buttons
-        .into_iter()
-        .map(|i| i.to_string())
-        .collect::<String>()
-}
-
-fn part2(lines: &Vec<&str>) -> String {
-    let mut buttons: Vec<char> = vec![];
-    let mut keypad = ComplexKeypad::new();
 
     for line in lines {
         buttons.push(keypad.process_line(line));
