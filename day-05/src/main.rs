147d52d4e4cd22a95723d342776df276ef3c0a54
tdor91 <thomas.dorfer@gmx.net>
2025-06-08 13:19:35 +0200

    improve code quality

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 fn brute_force_part1(input: &str) -> String {
     let mut result = Vec::new();
 
-    for index in 1.. {
+    for index in 0.. {
         let hash = hash_hex(input, index);
 
         if hash.starts_with("00000") {
@@ -30,7 +30,7 @@ fn brute_force_part2(input: &str) -> String {
     // Use an infinite loop instead of a while with condition:
     // We can reduce the number of times we have to check the condition by checking
     // inside the loop only when we found a new character. This should improve performance.
-    for index in 1.. {
+    for index in 0.. {
         let hash = hash_hex(input, index);
 
         if hash.starts_with("00000") {
